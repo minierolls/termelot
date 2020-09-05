b25a2b2dd476720274398d99bba9763a1ba667eb
minierolls <minierolls@gmail.com>
2020-09-05 11:01:55 -0700

    <fix> Improved donut example
    Now uses 24 bit color so that it also works on Windows, and
    changed math to account for cells being taller than they are
    wide.

--- examples/donut.zig
+++ examples/donut.zig
@@ -1,8 +1,5 @@
 //! This example is a port of Andy Sloane's donut renderer.
 //! https://www.a1k0n.net/2006/09/15/obfuscated-c-donut.html
-//! This example does not yet work on the Windows backend, as
-//! Windows does not support 8 bit color. This can be easily
-//! ported (and will be ported) to use 24-bit color instead.
 
 const std = @import("std");
 
@@ -30,7 +27,7 @@ pub fn main() !void {
     try term.setTitle("Termelot Donut");
 
     const screen_width: f32 = 60.0;
-    const screen_height: f32 = 60.0;
+    const screen_height: f32 = 20.0;
 
     const theta_spacing: f32 = 0.07;
     const phi_spacing: f32 = 0.02;
@@ -39,7 +36,7 @@ pub fn main() !void {
     const R2: f32 = 2.0;
     const K2: f32 = 5.0;
 
-    const K1: f32 = screen_width * K2 * 3.0 / (8.0 * (R1 + R2));
+    const K1: f32 = screen_height * K2 * 3.0 / (8.0 * (R1 + R2));
 
     var A: f32 = 0.0;
     var B: f32 = 0.0;
@@ -66,7 +63,7 @@ pub fn main() !void {
                 const circle_x = R2 + R1 * cos_theta;
                 const circle_y = R1 * sin_theta;
 
-                const x = circle_x *
+                const x = 2.0 * circle_x *
                     (cos_B * cos_phi + sin_A * sin_B * sin_phi) -
                     circle_y * cos_A * sin_B;
                 const y = circle_x *
@@ -75,8 +72,14 @@ pub fn main() !void {
                 const z = K2 + cos_A * circle_x * sin_phi + circle_y * sin_A;
                 const ooz = 1.0 / z;
 
-                const xp: u16 = @floatToInt(u16, @floor(screen_width / 2 + K1 * ooz * x));
-                const yp: u16 = @floatToInt(u16, @floor(screen_height / 2 - K1 * ooz * y));
+                const xp: u16 = @floatToInt(
+                    u16,
+                    @floor(screen_width / 2 + K1 * ooz * x),
+                );
+                const yp: u16 = @floatToInt(
+                    u16,
+                    @floor(screen_height / 2 - K1 * ooz * y),
+                );
 
                 const L = cos_phi * cos_theta * sin_B -
                     cos_A * cos_theta * sin_phi -
@@ -87,29 +90,29 @@ pub fn main() !void {
                     const lum: u8 = @floatToInt(u8, @floor(L * 8.0));
                     var c: Color = undefined;
                     if (lum == 0) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 55 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(7, 72, 146) };
                     } else if (lum == 1) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 56 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(22, 87, 161) };
                     } else if (lum == 2) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 57 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(34, 101, 178) };
                     } else if (lum == 3) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 91 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(46, 111, 185) };
                     } else if (lum == 4) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 92 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(58, 120, 192) };
                     } else if (lum == 5) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 93 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(72, 131, 199) };
                     } else if (lum == 6) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 127 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(88, 147, 215) };
                     } else if (lum == 7) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 128 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(99, 154, 218) };
                     } else if (lum == 8) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 129 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(113, 167, 229) };
                     } else if (lum == 9) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 163 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(124, 176, 235) };
                     } else if (lum == 10) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 164 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(140, 187, 240) };
                     } else if (lum == 11) {
-                        c = Color{ .Bit8 = ColorBit8{ .code = 165 } };
+                        c = Color{ .Bit24 = ColorBit24.initRGB(160, 202, 249) };
                     }
                     term.setCell(
                         Position{ .row = @intCast(u16, yp), .col = @intCast(u16, xp) },
