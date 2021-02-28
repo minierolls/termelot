3a3224a5f880338b98e30502374790d028ab527a
Luke Wilson <thelukaswils@gmail.com>
2021-02-28 17:11:49 -0600

    Ncurses backend development progress (incomplete)

--- src/backend/ncurses.zig
+++ src/backend/ncurses.zig
@@ -19,42 +19,66 @@ const Rune = termelot_import.Rune;
 const Style = termelot_import.style.Style;
 const Color = termelot_import.style.Color;
 const Decorations = termelot_import.style.Decorations;
+const Event = termelot_import.event.Event;
+
+// Zig couldn't translate these macros, I don't blame it:
+
+// #define getyx(win,y,x)		(y = getcury(win), x = getcurx(win))
+fn getyx(win: *c.WINDOW) Position {
+    return Position{
+        .row = @intCast(u16, c.getcury(win)),
+        .col = @intCast(u16, c.getcurx(win)),
+    };
+}
 
 pub const Backend = struct {
-    const Self = @This();
-
+    current_window: *c.WINDOW, // should be used in all ncurses function calls
+    alternate_window: ?*c.WINDOW,
     in_raw_mode: bool,
+    has_color: bool, // Whether this terminal supports colored output
+    cursor_visible: bool,
+
+    const Self = @This();
 
     /// Initialize backend
     pub fn init(
-        termelot: *Termelot,
         allocator: *std.mem.Allocator,
         config: Config,
     ) !Backend {
-        c.initscr();
-
-        if (c.has_colors()) c.start_color();
+        _ = c.initscr();
+        var has_color = false;
+        if (c.start_color() == c.OK) {
+            has_color = c.has_colors();
+            _ = c.use_default_colors(); // color pair -1 is now the terminal default pair
+        }
 
         return Backend{
+            .current_window = c.stdscr,
+            .alternate_window = null,
             .in_raw_mode = false,
+            .has_color = has_color,
+            .cursor_visible = true,
         };
     }
 
     /// Deinitialize backend
     pub fn deinit(self: *Self) void {
-        c.endwin();
+        // Reset attributes
+        _ = c.attroff(c.COLOR_PAIR(1));
+        _ = c.attron(c.A_NORMAL);
+
+        _ = c.endwin();
     }
 
     /// Retrieve supported features for this backend.
     pub fn getSupportedFeatures(self: *Self) !SupportedFeatures {
-        const has_colors = c.has_colors();
         return SupportedFeatures{
             .color_types = .{
-                .Named16 = has_colors,
-                .Bit8 = has_colors,
-                .Bit24 = has_colors,
+                .Named16 = self.has_color,
+                .Bit8 = self.has_color and c.COLORS <= 256,
+                .Bit24 = self.has_color,
             },
-            .decorations = Style.Decorations{
+            .decorations = Decorations{
                 .bold = true,
                 .italic = true,
                 .underline = true,
@@ -70,19 +94,20 @@ pub const Backend = struct {
 
     /// Enter/exit raw mode.
     pub fn setRawMode(self: *Self, enabled: bool) !void {
+        // TODO: how is raw mode disabled supposed to work with pollEvent??
         if (enabled) {
-            c.cbreak(); // Disable line-based buffering
-            c.noecho(); // Disable echoing user input
-            c.nonl(); // Disable enter key generating new-line characters
-            c.intrflush(c.stdscr, false); // Prevent flush on interrupts
-            c.keypad(c.stdscr, true); // Treat control / arrow / function keys specially??
+            _ = c.cbreak(); // Disable line-based buffering
+            _ = c.noecho(); // Disable echoing user input
+            _ = c.nonl(); // Disable enter key generating new-line characters
+            _ = c.intrflush(c.stdscr, false); // Prevent flush on interrupts
+            _ = c.keypad(c.stdscr, true); // Treat control / arrow / function keys specially??
         } else {
             // TODO: default to the tty driver settings when disabling raw mode
-            c.nocbreak(); // Enable line-based buffering
-            c.echo(); // Enable echoing user input
-            c.nl(); // Enter key generates new-lines "\n"
-            c.intrflush(c.stdscr, true);
-            c.keypad(c.stdscr, false);
+            _ = c.nocbreak(); // Enable line-based buffering
+            _ = c.echo(); // Enable echoing user input
+            _ = c.nl(); // Enter key generates new-lines "\n"
+            _ = c.intrflush(c.stdscr, true);
+            _ = c.keypad(c.stdscr, false);
         }
     }
 
@@ -91,81 +116,104 @@ pub const Backend = struct {
     /// If timeout is greater than zero:
     /// Non-blocking; return next available Event if one arises within `timeout` ms.
     pub fn pollEvent(self: *Self, timeout: i32) !?Event {
-        @compileError("Unimplemented");
+        // TODO
+        return error.Unimplemented;
     }
 
     /// Retrieve alternate screen status.
     pub fn getAlternateScreen(self: *Self) !bool {
-        @compileError("Unimplemented");
+        return self.current_window != c.stdscr;
     }
 
     /// Enter/exit alternate screen.
     pub fn setAlternateScreen(self: *Self, enabled: bool) !void {
-        @compileError("Unimplemented");
-    }
-
-    /// Start event/signal handling loop, non-blocking immediate return.
-    pub fn start(self: *Self) !void {
-        // This function should call necessary functions for screen size
-        // update, key event callbacks, and mouse event callbacks.
-        @compileError("Unimplemented");
-    }
-
-    /// Stop event/signal handling loop.
-    pub fn stop(self: *Self) void {
-        @compileError("Unimplemented");
+        if (enabled) {
+            if (self.alternate_window == null) {
+                // TODO: no alt window, we must create one
+            }
+            // self.current_window = self.alternate_window orelse unreachable;
+        } else {
+            self.current_window = c.stdscr;
+        }
+        // TODO
     }
 
     /// Set terminal title.
     pub fn setTitle(self: *Self, runes: []const Rune) !void {
-        @compileError("Unimplemented");
+        // TODO however termios does it
     }
 
     /// Get screen size.
     pub fn getScreenSize(self: *Self) !Size {
-        // This function will only be called once on
-        // startup, and then the size should be set
-        // through the event handling loop.
-        @compileError("Unimplemented");
+        return Size{
+            .rows = @intCast(u16, c.getmaxy(c.stdscr)) + 1,
+            .cols = @intCast(u16, c.getmaxx(c.stdscr)) + 1,
+        };
     }
 
     /// Get cursor position.
     pub fn getCursorPosition(self: *Self) !Position {
-        @compileError("Unimplemented");
+        return getyx(c.stdscr); // Defined at the top
     }
 
     /// Set cursor position.
     pub fn setCursorPosition(self: *Self, position: Position) !void {
-        @compileError("Unimplemented");
+        _ = c.move(position.row, position.col); // TODO: make sure we can discard this return code
     }
 
     /// Get cursor visibility.
     pub fn getCursorVisibility(self: *Self) !bool {
-        @compileError("Unimplemented");
+        return self.cursor_visible;
     }
 
     /// Set cursor visibility.
     pub fn setCursorVisibility(self: *Self, visible: bool) !void {
-        @compileError("Unimplemented");
+        self.cursor_visible = visible;
+        _ = c.curs_set(@intCast(c_int, @boolToInt(visible)));
     }
 
     /// Parse a Style and using ncurses' attrset, set terminal attributes.
-    fn attrset(self: *Self, style: *const Style) !void {
+    fn setAttributes(self: *Self, style: *const Style) void {
         // NOTE: use wattr to operate on a specific WINDOW*
-        var attr: c_short = 0;
+        var attr: c_uint = 0;
 
         if (style.decorations.bold) attr |= c.A_BOLD;
         if (style.decorations.italic) attr |= c.A_ITALIC; // NOTE: A_ITALIC is a feature of 2013's ncurses 5.10.
         if (style.decorations.underline) attr |= c.A_UNDERLINE;
         if (style.decorations.blinking) attr |= c.A_BLINK;
 
-        const _ = Color;
+        _ = c.wattrset(c.stdscr, @intCast(c_int, attr));
 
-        // set color pair before use
-        switch (style.bg_color) {
-            
+        // if (!self.has_color) return;
+
+        // NOTE: see description of `man 3 init_pair` about converting pairs to video attributes...
+
+        // default colors
+        // TODO: copy colors from pair -1 (terminal defaults) to these attributes (using attr_get)
+        var fg_color: c_int = 7; // foreground white
+        var bg_color: c_int = 0; // background black
+
+        // TODO: hypothesis is that colors 0-7 are set, and all others are not... lol
+
+        switch (style.fg_color) {
+            .Default => {},
+            .Named16 => |v| fg_color = @enumToInt(v), // do good
+            .Bit8 => |v| fg_color = v.code, // questionable
+            .Bit24 => |v| fg_color = v.roundToBit8().code, // uh oh
         }
-        // attrset(COLOR_PAIR(1))
+
+        switch (style.bg_color) {
+            .Default => {},
+            .Named16 => |v| bg_color = @enumToInt(v),
+            .Bit8 => |v| bg_color = v.code,
+            .Bit24 => |v| bg_color = v.roundToBit8().code, // TODO: RGB (probably have to use ANSI escape sequence manually)
+        }
+
+        // std.log.debug("{}", .{fg_color});
+
+        // TODO: I believe init-ing pairs resets all colors on the screen with that pair????
+        _ = c.init_extended_pair(1, fg_color, bg_color); // we always reassign pair 1
+        _ = c.wattr_on(c.stdscr, @intCast(c_uint, c.COLOR_PAIR(1)), c.NULL); // make sure it is set
     }
 
     /// Write styled output to screen at position. Assumed that no newline
@@ -177,17 +225,18 @@ pub const Backend = struct {
         styles: []Style,
     ) !void {
         // move to position
-        c.move(position.row, position.col);
+        _ = c.wmove(c.stdscr, position.row, position.col);
         for (runes) |rune, i| {
             // set styling
-            self.attrset(&styles[i]);
+            self.setAttributes(&styles[i]);
             // write next character
-            c.addch(rune); // NOTE: addch adds character to window stdscr, use waddch for other c.WINDOW*
+            // TODO: make sure this supports UTF-8
+            _ = c.waddch(c.stdscr, rune); // NOTE: addch adds character to window stdscr, use waddch for other c.WINDOW*
         }
 
         // NOTE: could use wchgat to set character attributes *after* writing characters to optimize styling...
 
-        c.refresh(); // Redraw terminal from what we've written to stdscr
+        _ = c.wrefresh(c.stdscr); // Redraw terminal from what we've written to stdscr
     }
 };
 
